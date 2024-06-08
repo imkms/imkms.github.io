6840ac5a9a1ee923c8bb9cd50e47c11cd2f593b5
imkms <egon.leko@gmail.com>
2024-06-08 23:00:45 +0200

    Nothing interesting

---
+++ script.js
@@ -0,0 +1,4 @@
+const downloadButton = document.querySelector('.download-button');
+downloadButton.addEventListener('click', () => {
+    // Add code here to play a click sound (e.g., using the Audio API)
+});
